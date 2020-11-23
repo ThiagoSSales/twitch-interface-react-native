bf16bb487ec457353f80c4497d12954c6be14b9c
ThiagoSSales <thiago.ss133@gmail.com>
2020-11-23 14:39:40 -0300

    Twitch interface in react native

---
+++ src/pages/ComingSoon/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { Text } from "react-native";
+
+import { Wrapper, Headding } from "./styles";
+
+const ComingSoon: React.FC = () => {
+  return (
+    <Wrapper>
+      <Headding>Coming soon...</Headding>
+    </Wrapper>
+  );
+};
+
+export default ComingSoon;
